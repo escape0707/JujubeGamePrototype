9f0c56ab32a6de1b58e1eeae6560e5a88f7d61bc
Jay Chu <tothesong@gmail.com>
2018-12-26 11:07:48 +0800

    JujubeBoard.cs

---
+++ Assets/Scripts/JujubeBoard.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class JujubeBoard : MonoBehaviour {
+	private CanvasGroup[] allCanvasGroups = null; // TOTHINK: Any way to encapsulate this var from me?
+
+	internal CanvasGroup[] AllCanvasGroups {
+		get {
+			if (allCanvasGroups == null) {
+				allCanvasGroups = GetComponentsInChildren<CanvasGroup>();
+			}
+			return allCanvasGroups;
+		}
+	}
+
+	internal void MakeAllGroupsInteractable() {
+		foreach (CanvasGroup canvasGroup in AllCanvasGroups) {
+			canvasGroup.interactable = true;
+		}
+	}
+
+	internal void MakeOtherGroupsNotInteractable(CanvasGroup theRemainingGroup) {
+		foreach (CanvasGroup canvasGroup in AllCanvasGroups) {
+			if (canvasGroup != theRemainingGroup) {
+				canvasGroup.interactable = false;
+			}
+		}
+	}
+}
