e5c8858e52ba80c5b68ef37a01d0174e3aec7c79
Jay Chu <tothesong@gmail.com>
2018-12-26 10:32:21 +0800

    JujubeToggle logic (incomplete)

---
+++ Assets/Scripts/JujubeGroup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class JujubeGroup : MonoBehaviour {
+	[SerializeField]
+	private GameObject JujubePrefab;
+
+	[SerializeField]
+	private int maxJujubeCountForGroup;
+	private int jujubeCount;
+	private int previousCount; // TODO!! Not maintained.
+
+	internal void PickOne() {
+		Assert.IsTrue(--jujubeCount >= 0, "Jujube count for this group goes to negative");
+	}
+
+	internal void ReturnOne() {
+		Assert.IsTrue(++jujubeCount <= maxJujubeCountForGroup, "Jujube count for this group exceeded its max");
+	}
+
+  internal bool NotPickedInThisTurn() {
+		return jujubeCount == previousCount;
+	}
+
+	void Awake() {
+		for (jujubeCount = 0; jujubeCount < maxJujubeCountForGroup; ++jujubeCount) {
+			Instantiate(JujubePrefab, transform);
+		}
+		previousCount = jujubeCount;
+	}
+}
---
+++ Assets/Scripts/JujubeToggle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JujubeToggle : MonoBehaviour {
+	private Toggle jujubeToggle;
+	private JujubeGroup groupController;
+
+	void Awake() {
+		jujubeToggle = GetComponent<Toggle>();
+		jujubeToggle.onValueChanged.AddListener(delegate {
+			ToggleValueChanged(jujubeToggle);
+		});
+
+		groupController = GetComponentInParent<JujubeGroup>();
+	}
+
+	void ToggleValueChanged(Toggle changedToggle) {
+		if (changedToggle.isOn) {
+			groupController.ReturnOne();
+			if (groupController.NotPickedInThisTurn()){
+				groupController.MakeAllGroupsInteractable();
+			}
+		} else {
+			groupController.PickOne();
+			MakeOtherGroupsNotInteractable();
+		}
+	}
+
+  private void MakeOtherGroupsNotInteractable()
+  {
+    throw new NotImplementedException();
+  }
+}
